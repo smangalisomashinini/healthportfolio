82b5c0cb1450a79d1c65cd89f5ccedbcbe2b0b57
Smangaliso Mashinini <124200553+smangalisomashinini@users.noreply.github.com>
2024-08-29 02:28:35 +0200

    Updated the page to include heartrate and heart disease images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,6 +4,8 @@ import project1 from '../assets/images/project1.jpg';
 import project2 from '../assets/images/project2.jpg';
 import project3 from '../assets/images/project3.jpg';
 import project4 from '../assets/images/project4.jpg';
+import project5 from '../assets/images/heartrate.jpg';
+import project6 from '../assets/images/heartattack.jpg';
 
 const Projects = () => {
     return (
@@ -34,6 +36,21 @@ const Projects = () => {
                     </a>
                     <h2>Diabetics</h2>
                 </div>
+            </div>
+            <div className="projects-grid">
+                <div className="project-item">
+                    <a href="https://huggingface.co/spaces/kamogeloVutomi/breathing" target="_blank" rel="noopener noreferrer">
+                        <img src={project5} alt="Project 1" />
+                    </a>
+                    <h2>Breathing rate monitoring</h2>
+                </div>
+                <div className="project-item">
+                    <a href="https://huggingface.co/spaces/KhumaloKat/Heart-Disease-Prediction" target="_blank" rel="noopener noreferrer">
+                        <img src={project6} alt="Project 2" />
+                    </a>
+                    <h2>heart disease prediction</h2>
+                </div>
+              
             </div>
         </div>
     );
