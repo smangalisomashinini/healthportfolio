5983d647c8628ee68fda7093e479b4ee6184cc68
Smangaliso Mashinini <124200553+smangalisomashinini@users.noreply.github.com>
2025-01-29 11:07:56 +0200

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -36,7 +36,7 @@ const Projects = () => {
                
                 
                 <div className="project-item">
-                    <a href="https://eacb719fd6ed35ca2e.gradio.live" target="_blank" rel="noopener noreferrer">
+                    <a href="https://huggingface.co/spaces/SiphoMahlangu/cancer" target="_blank" rel="noopener noreferrer">
                         <img src={project3} alt="Project 3" />
                     </a>
                     <h2>Skin cancer detection</h2>
